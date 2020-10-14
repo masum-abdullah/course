f135307bd606a34b2ff0ca5c275ed4d035581897
masum-abdullah <masumbhuiyan00000@gmail.com>
2020-10-14 20:11:50 +0600

    first commit

---
+++ Coursedata.js
@@ -0,0 +1,142 @@
+var Courses = [
+    {
+        name='Complete Web Development'
+        fees='20'
+        instractor='Jhankar Mahbub'
+        duration='120'
+        img='https://www.tutorialrepublic.com/lib/images/javascript-illustration.png'
+    }
+    {
+        name='English for Career Development'
+        fees='2'
+        instractor='Wadud Ahmed (raju)'
+        duration='5'
+        img='https://i1.wp.com/ritajms.com/wp-content/uploads/2019/11/52351011-english-british-england-language-education-concept.jpg?resize=768%2C567&ssl=1'
+    }
+    {
+        name='Data Structures and Algorithms Specialization'
+        fees='35'
+        instractor='john nill'
+        duration='150'
+        img='https://miro.medium.com/max/700/1*sMryEXZVPKFjGNcfSzE8Mw.jpeg'
+    }
+    {
+        name='IBM Cybersecurity Analyst Professional Certificate'
+        fees='50'
+        instractor='cyber 71'
+        duration='90'
+        img='https://specials-images.forbesimg.com/imageserve/5e180c9d735f8c0007998956/960x0.jpg?fit=scale'
+    }
+    {
+        name='Spread peace'
+        fees='Free'
+        instractor='Maulana Tariq jamil & Dr. Zakir Nayak'
+        duration='5'
+        img='https://image.shutterstock.com/image-vector/lailahaillallahmuhammadurrasulullah-design-islamic-holidays-this-600w-1039615639.jpg'
+    }
+    {
+        name='HTML, CSS, and Javascript for Web Developers'
+        fees='15'
+        instractor='Jhankar Mahbub'
+        duration='70'
+        img='https://www.tutorialrepublic.com/lib/images/javascript-illustration.png'
+    }
+    {
+        name='Wanna be a fasist like me?'
+        fees='20000'
+        instractor='Xi Jinping'
+        duration='2'
+        img='https://cdn1.i-scmp.com/sites/default/files/styles/1200x800/public/images/methode/2018/01/15/e2841738-f9cf-11e7-b2f7-03450b80c791_image_hires_171232.jpg?itok=3Y3xlpjs&v=1516007556'
+    }
+    {
+        name='degree in Computer Science'
+        fees='1000'
+        instractor='University Of London'
+        duration='950'
+        img='https://scholarship-positions.com/wp-content/uploads/2020/05/100-Full-Tuition-V%C3%A1radi-Scholarships-for-International-Students-768x448.jpg'
+    }
+    {
+        name='An Introduction to American Law'
+        fees='8'
+        instractor='Rashedul Islam'
+        duration='8'
+        img='https://www.acq-intl.com/wp-content/uploads/2019/12/Kettani-Law-Firm-1.jpg'
+    }
+    {
+        name='Master of Science in Electrical Engineering'
+        fees='400'
+        instractor='University of Colorado Boulder'
+        duration='300'
+        img='http://www.gobeyondthebrochure.com/wp-content/uploads/2015/07/5-Things-I-Wish-I-Knew-Before-Attending-University-of-Colorado-Boulder.jpg'
+    }
+    {
+        name='Stanford Introduction to Food and Health'
+        fees='free'
+        instractor='Javed Karim'
+        duration='3'
+        img='https://studentgiri.com/wp-content/uploads/2019/08/32a6acec-food-and-health.jpg'
+    }
+    {
+        name='Financial Markets'
+        fees='4'
+        instractor='Steve Smith'
+        duration='4'
+        img='https://miro.medium.com/max/1500/1*7YFoQteQxc4Pa9a4GEZ5xQ.jpeg'
+    }
+    {
+        name='First Step Korean'
+        fees='5'
+        instractor='Xiaoyu Liu'
+        duration='1.5'
+        img='https://cdnuploads.aa.com.tr/uploads/Contents/2020/07/14/thumbs_b_c_b5dfefa3c1bc76520f5f12d644de9e1a.jpg?v=144318'
+    }
+    {
+        name='Greek and Roman Mythology'
+        fees='2'
+        instractor='peter parker'
+        duration='1'
+        img='https://greece.greekreporter.com/files/Greek-roman-deities.jpg'
+    }
+    {
+        name='Spatial Data Analysis and Visualization MasterTrack™ Program'
+        fees='3'
+        instractor='Superman'
+        duration='1'
+        img='https://spatial.usc.edu/wp-content/uploads/2020/04/Audience-Engagement-Venn-Diagram.jpg'
+    }
+    {
+        name='Étudier en France: French Intermediate course B1-B2'
+        fees='5'
+        instractor='Monsur Ali Khan'
+        duration='2'
+        img='https://www.etudiant.ma/uploads/article/cover/10466/large_head-campus-fr-1.png'
+    }
+    {
+        name='Learn German Language'
+        fees='15'
+        instractor='lindsey lohan'
+        duration='1'
+        img='https://www.languagelegos.com/wp-content/uploads/2018/09/17-reasons-to-learn-German-language..-1080x675.jpg'
+    }
+    {
+        name='Complete Photograohy Course'
+        fees='1'
+        instractor='Mahia Tarafdar'
+        duration='3'
+        img='https://aseanop.com/wp-content/uploads/2020/04/Study-Online-Photography-Course-For-Free.jpg'
+    }
+    {
+        name="Learn World's Reachest Language Bangla"
+        fees='4'
+        instractor='Rubaiya Sadat'
+        duration='4'
+        img='https://assetsds.cdnedge.bluemix.net/sites/default/files/beta2/uploads/2013/03/Page-2011-325x240.jpg'
+    }
+    {
+        name='Chinese for Beginners'
+        fees='Free'
+        instractor='Ho Chi Minn'
+        duration='6'
+        img='https://scontent.fdac5-1.fna.fbcdn.net/v/t1.0-9/s960x960/117342711_3233772766717000_756455452818767148_o.jpg?_nc_cat=111&_nc_sid=dd9801&_nc_ohc=pUGO30tA480AX9nPFIQ&_nc_ht=scontent.fdac5-1.fna&tp=7&oh=2eb8c5690842ebb1fdd99724f3f42c31&oe=5FA9F2AF'
+    }
+];
